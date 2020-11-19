388cc270ad5308489901027f3e0c7c442539e5fe
Juan Sanchez <juansanchez721@outlook.com>
2020-11-19 16:02:51 -0500

    Finished #two_sum Specs and definition.

--- Test_driven_development/lib/tdd.rb
+++ Test_driven_development/lib/tdd.rb
@@ -9,10 +9,23 @@ def my_uniq
     uniq_hash.keys
 end
 
+
+# arr = [0,0,0,1,2,2,3]
+
+
+# p arr.my_uniq
+
+def two_sum
+    results = []
+    self.each_with_index do |ele, i|
+        self.each_with_index do |el, j|
+            if j > i && self[i] + self[j] == 0
+                results << [i, j]
+            end
+        end
+    end
+    []
+    results
 end
 
-arr = [0,0,0,1,2,2,3]
-
-
-p arr.my_uniq
-
+end
--- Test_driven_development/spec/tdd_spec.rb
+++ Test_driven_development/spec/tdd_spec.rb
@@ -16,7 +16,21 @@ RSpec.describe Array do
             test_arr.my_uniq
             expect(test_arr.my_uniq).to eq([0, 1])
         end
-
-
     end
+
+    describe "#two_sum" do
+
+        it "should not modify the original array" do
+            test_arr = [0, -1, 3, 0, 1, 5, -6, 6]
+            test_arr.two_sum
+            expect(test_arr).to eq([0, -1, 3, 0, 1, 5, -6, 6])
+        end
+
+        it "should return all pairs of an array that sum to 0" do
+            test_arr = [0, -1, 3, 0, 1, 5, -6, 6]
+            expect(test_arr.two_sum).to eq([[0,3], [1,4], [6,7]])
+        end
+        
+    end
+
 end
